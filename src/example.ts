e3422fb8963eeb64bdc952343e043c55f45118a0
eyhn <cneyhn@gmail.com>
2021-06-28 17:29:49 +0800

    fix: click to play

--- src/example.ts
+++ src/example.ts
@@ -4,24 +4,30 @@ import testSound from "./static/cha.mp3";
 
 import bgImg from "./static/9s.jpg";
 
-const azusa = new Azusa({
-  view: document.getElementById('app') as HTMLCanvasElement,
-  subdivisionSize: 1024,
-  cutEnd: 256
-});
+function start() {
+  console.log('start')
+  const azusa = new Azusa({
+    view: document.getElementById('app') as HTMLCanvasElement,
+    subdivisionSize: 1024,
+    cutEnd: 256
+  });
+  
+  azusa.audio.load({
+    src: testSound,
+    onPrgress: (xhr: ProgressEvent) => {
+      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    }
+  });
+  
+  azusa.audio.setVolume(0.5);
+  
+  window.addEventListener('resize', () => {
+    azusa.resize(window.innerWidth, window.innerHeight);
+  })
+  
+  const container = document.getElementById('bg')
+  container && (container.style.backgroundImage = `url('${bgImg}')`)
+  window.removeEventListener('click', start)
+}
 
-azusa.audio.load({
-  src: testSound,
-  onPrgress: (xhr: ProgressEvent) => {
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  }
-});
-
-azusa.audio.setVolume(0.5);
-
-window.addEventListener('resize', () => {
-  azusa.resize(window.innerWidth, window.innerHeight);
-})
-
-const container = document.getElementById('bg')
-container && (container.style.backgroundImage = `url('${bgImg}')`)
+window.addEventListener('click', start)
